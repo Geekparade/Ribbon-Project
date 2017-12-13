54f557a566254d7f673c3360c6e9a17400d8777e
Mathéo <matheo.zeller@gmail.com>
2017-12-13 11:55:42 +0100

    Création css

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// function chest(){
+// 	if(".usbko")
+
+$(".usbok").hide();
+$(".reseauok").hide();
+$(".mario").hide();
+
+
+
+
+// function main(){
+// 	$(".usbko").animate()
+
+
+
+// }();
+
+// $(function()
+// {
+//   var expanded = false;
+//   $('#sidebar').click(function()
+//                       {
+//                           if (!expanded)
+//                           {
+//                               $(this).animate({'left' : '0px'}, {duration : 400});
+//                               expanded = true;
+//                           }
+//                           else
+//                           {
+//                              $(this).animate({'left' : '565px'}, {duration: 400});
+//                               expanded = false;
+//                           }
+//                       });
+//  });
